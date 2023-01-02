8915a8d1c4b3b10d64973dff2f4681208e1d8f6d
kajal <kajalfegade28@gmail.com>
2023-01-02 06:34:16 +0530

    hello pari

---
+++ paro10/src/test/java/paro1020/clone.java
@@ -0,0 +1,10 @@
+package paro1020;
+
+public class clone {
+	public static void main(String[] args) {
+		
+		System.out.println("heionckljkln,ndlkwkll");
+		System.out.println("hello good morning");
+	}
+
+}
---
+++ paro10/src/test/java/paro1020/clone2.java
@@ -0,0 +1,10 @@
+package paro1020;
+
+public class clone2 {
+
+	public static void main(String[] args) {
+
+		System.out.println("hello good evening");
+	}
+
+}
